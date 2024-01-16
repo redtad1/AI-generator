fe29726375218e271b96a3d5143623cb15043491
redtad1 <rediat1b@gmail.com>
2024-01-15 22:14:00 -0600

    java started

--- src/index.js
+++ src/index.js
@@ -1,13 +1,25 @@
 function generate(event) {
   event.preventDefault();
+
+  new Typewriter("#recipegenerstor", {
+    String: response.data.answer,
+    autoStart: true,
+    delay: 1,
+    cursor: "",
+  });
+
   let apikey = "c43da09a1983b5385088td638a5of324";
-  let context =
-    "you are a funny AI assistant that tells short and sweet Jokes. the joke must be provided in HTML format. Example:<p>this is a joke</p>";
-  let prompt = "Generate a short and funny joke about chickens.";
+  let context = "Be polite. Follow user instructions:";
+  let prompt = `User instructions: Generate 1 cookie recipe with the main ingredient of ${userInput.value}. Do not introduce yourself. Separate each sentence with <br/>. Separate each section with <br/>. Display the recipe title in <strong> element. Display the recipe ingredients in <strong> element. Display recipe instructions in <strong> element. Sign the recipe with <br/> element as "Generated with ❤️ by SheCodes AI" in a <strong> element`;
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apikey}`;
 
-  let jokeElement = document.querySelector("#joke");
-  jokeElement.innerHTML = "Getting ready 🥸 plaese wait";
+  let promptRecipeElement = document.querySelector("#prompt-recipe");
+  promptRecipeElement.addEventListener("submit", generateRecipe);
+
+  let recipeGeneratorElement = document.querySelector("#recipe-generator");
+  recipeGeneratorElement.innerHTML = `<span class="blink">Generating recipe for a ${userInput.value}cookies ...</span>`;
+
+  console.log;
 
   console.log("called the. AI api");
   axios.get(apiUrl).then(displayJoke);
