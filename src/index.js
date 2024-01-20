00be02356600d8ebc085f0d59d699897a8ac2cfa
redtad1 <rediat1b@gmail.com>
2024-01-19 19:55:41 -0600

    worked on java script.

--- src/index.js
+++ src/index.js
@@ -1,27 +1,27 @@
-function generate(event) {
-  event.preventDefault();
-
+function displayRecipe(response) {
   new Typewriter("#recipegenerstor", {
-    String: response.data.answer,
+    Strings: [response.data.answer],
     autoStart: true,
     delay: 1,
     cursor: "",
   });
-
+}
+function generateRecipe(event) {
+  event.preventDefault();
+  let instructionsInput = document.querySelector("#user-instructions");
   let apikey = "c43da09a1983b5385088td638a5of324";
-  let prompt = `Generate a delicious cookie recipe about ${instructionsInput.value}`;
-  let context =
-    "You are a baker that likes short and simple recipes in basic HTML and seperate each line always with <br/>. Please include the title in an <h3>";
+  let prompt = `User instructions:Generate a delicious cookie recipe ${instructionsInput.value}`;
+  let context = `You are a baker that likes short and simple cookie recipes in basic HTML and separate each line always with <br/>. Please include the title in an <h3>`;
 
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apikey}`;
+  console.log(`prompt:${prompt}`);
+  console.log(`context:${context}`);
 
-  let promptRecipeElement = document.querySelector("#prompt-recipe");
-  recipeElement.classList.remove("hide-element");
-  promptRecipeElement.addEventListener("submit", generateRecipe);
-
-  let recipeGeneratorElement = document.querySelector("#recipe-generator");
-  recipeGeneratorElement.innerHTML = `<div class="recipe">Generating recipe for a ${userInput.value}cookies ...</div>`;
-
-  console.log("called the. AI api");
-  axios.get(apiUrl).then(displayJoke);
+  axios.get(apiUrl).then(displayRecipe);
 }
+
+let promptRecipeElement = document.querySelector("#prompt-recipe");
+promptRecipeElement.addEventListener("submit", generateRecipe);
+
+let recipeGeneratorElement = document.querySelector("#recipe-generator");
+recipeGeneratorElement.innerHTML = `<div class="generating">👩‍🍳 Here's a delicious cookie recipe for you🍪 !</div>`;
